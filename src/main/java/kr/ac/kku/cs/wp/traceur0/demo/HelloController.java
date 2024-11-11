5ce9fbb3d3f99d77db3f6d1f5177374d09437ce9
Desktop <ghost01ghost@gmail.com>
2024-11-12 01:03:02 +0900

    first commit

---
+++ src/main/java/kr/ac/kku/cs/wp/traceur0/demo/HelloController.java
@@ -0,0 +1,25 @@
+package kr.ac.kku.cs.wp.traceur0.demo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+public class HelloController {
+	
+	private static final Logger logger = LogManager.getLogger(HelloController.class);
+	
+	@RequestMapping("/hello")
+	public ModelAndView hello(@RequestParam(name = "name", required = true) String name) {
+		
+		final String formattedGreeting = "hello %s";
+		String greeting = formattedGreeting.formatted(name);
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("hello", greeting);
+		mav.setViewName("HelloView");
+		
+		return mav;
+	}
+}
